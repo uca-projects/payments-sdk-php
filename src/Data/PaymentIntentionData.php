ce50803884a07f6e3d105eb30f8f5716e1e7b91c
Gabriel Esteban Teruel <gabriel_teruel@uca.edu.ar>
2025-10-09 23:50:31 +0000

    Agrego nuevos DTOs

---
+++ src/Data/PaymentIntentionData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Uca\PaymentsSharedClass\Data;
+
+use OpenApi\Attributes as OA;
+use Spatie\LaravelData\Data;
+
+#[OA\Schema(
+    schema: 'PaymentIntentionData',
+    title: 'PaymentIntentionData',
+    description: 'Payment intention attributes',
+    properties: [
+        new OA\Property(property: 'transaction_intent_id', type: 'string', nullable: true),
+        new OA\Property(property: 'checkout_url', type: 'string', nullable: true),
+        new OA\Property(property: 'qr', type: 'string', nullable: true),
+        new OA\Property(property: 'deep_link', type: 'string', nullable: true),
+        new OA\Property(property: 'notification_url', type: 'string', nullable: true),
+        new OA\Property(property: 'back_urls', type: 'string', nullable: true),
+    ]
+)]
+class PaymentIntentionData extends Data
+{
+    /**
+     * @param string|null $transaction_intent_id
+     * @param string|null $checkout_url
+     * @param string|null $qr
+     * @param string|null $deep_link
+     * @param string|null $notification_url
+     * @param string|null $back_urls
+     */
+    public function __construct(
+        public ?string $transaction_intent_id,
+        public ?string $checkout_url,
+        public ?string $qr,
+        public ?string $deep_link,
+        public ?string $notification_url,
+        public ?string $back_urls,
+    ) {}
+}
